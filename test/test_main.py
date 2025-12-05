d7c471f3ed821c8ec8570c24a32e3bce04ffc270
RafaelOrge92 <rafaelorge92@gmail.com>
2025-12-05 09:18:08 +0100

    tercer commit

--- test/test_main.py
+++ test/test_main.py
@@ -1,4 +1,7 @@
-from src.main import elevar
+from src.main import elevar, to_upper
 
 def test_elevar():
     assert elevar(5,5) == 3125
+    
+def test_to_upper():
+    assert to_upper("rafael") == "RAFAEL"
